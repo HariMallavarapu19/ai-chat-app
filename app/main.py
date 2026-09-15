b950b42c472b3a93a85181f992a8cfa8f4a7adfd
HariMallavarapu <purnamallavarapu19@gmail.com>
2026-09-15 12:50:54 +0530

    Add user registration

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
-from app.database import create_db_and_tables
+from fastapi import FastAPI,HTTPException
 from contextlib import asynccontextmanager
+from sqlmodel import Session,select
+from app.database import create_db_and_tables,engine
+from app.auth import hash_password
+from app.models import User
+from app.schemas import UserCreate
+
 
 
 
@@ -15,6 +20,35 @@ app=FastAPI(lifespan=lifespan)
 @app.get('/')
 def home():
     return {"message":"Ai chat app is running"}
+
+@app.post("/register")
+def register(user:UserCreate):
+    hashed_password=hash_password(user.password)
+
+    new_user=User(
+        username=user.username,
+        email=user.email,
+        hashed_password=hashed_password
+    )
+
+    with Session(engine) as session:
+        existing_user=session.exec(
+            select(User).where(
+                (User.username==user.username)|(User.email==user.email)
+            )
+        ).first()
+
+        if existing_user:
+            raise HTTPException(status_code=400,detail="already registerd")
+        session.add(new_user)
+        session.commit()
+        session.refresh(new_user)
+
+    return {
+        "message":"User created Successfully",
+        "username":new_user.username,
+        "email":new_user.email
+    }
 
 
 
