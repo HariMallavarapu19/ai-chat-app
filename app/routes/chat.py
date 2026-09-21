4ff254634dfec7958876b9db3f545e9078832881
HariMallavarapu <purnamallavarapu19@gmail.com>
2026-09-21 12:31:58 +0530

    Add chat and message tests

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends,HTTPException
 from sqlmodel import Session,select
 from app.services.gemini import ask_gemini_with_history,ask_gemini
 from app.auth import get_current_user
-from app.database import engine
+from app.database import engine,get_session
 from app.models import Chat, User, Message
 from app.schemas import (MessageCreate,
 MessageResponse,CreateMessageResponse,ChatResponse,MessageResponseSimple)
@@ -17,16 +17,17 @@ router = APIRouter(
 @router.post("/",response_model=ChatResponse)
 def create_chat(
     title: str,
-    current_user: User = Depends(get_current_user)):
+    current_user: User = Depends(get_current_user),
+    session:Session=Depends(get_session)):
     new_chat = Chat(
         user_id=current_user.id,
         title=title
     )
 
-    with Session(engine) as session:
-        session.add(new_chat)
-        session.commit()
-        session.refresh(new_chat)
+
+    session.add(new_chat)
+    session.commit()
+    session.refresh(new_chat)
 
     return {
         "id": new_chat.id,
@@ -36,37 +37,45 @@ def create_chat(
     }
 
 @router.get('/',response_model=list[ChatResponse])
-def get_chats(current_user:User=Depends(get_current_user)):
-    with Session(engine) as session:
-        chats=session.exec(
-            select(Chat).where(Chat.user_id==current_user.id)
-        ).all()
+def get_chats(current_user:User=Depends(get_current_user),
+              session:Session=Depends(get_session)):
+
+    chats=session.exec(
+        select(Chat).where(Chat.user_id==current_user.id)
+    ).all()
 
     return chats
 
 @router.get('/{chat_id}',response_model=ChatResponse)
-def get_chat(chat_id:int,current_user:User=Depends(get_current_user)):
-    with Session(engine) as session:
-        chat=session.exec(
-            select(Chat).where((Chat.id==chat_id) & (Chat.user_id==current_user.id))
-        ).first()
+def get_chat(chat_id:int,
+             current_user:User=Depends(get_current_user),
+             session:Session=Depends(get_session)
+             ):
+    
 
-        if chat is None:
-            raise HTTPException(status_code=404,detail='chat not found')
+    chat=session.exec(
+        select(Chat).where((Chat.id==chat_id) & (Chat.user_id==current_user.id))
+    ).first()
+
+    if chat is None:
+        raise HTTPException(status_code=404,detail='chat not found')
+    
     return chat
 
 @router.delete('/{chat_id}',response_model=MessageResponseSimple)
-def delete_chat(chat_id:int,current_user:User=Depends(get_current_user)):
-    with Session(engine) as session:
-        chat=session.exec(
-            select(Chat).where((Chat.id==chat_id)&(Chat.user_id==current_user.id))
-        ).first()
+def delete_chat(chat_id:int,
+                current_user:User=Depends(get_current_user),
+                session:Session=Depends(get_session)):
+    
+    chat=session.exec(
+        select(Chat).where((Chat.id==chat_id)&(Chat.user_id==current_user.id))
+    ).first()
 
-        if chat is None:
-            raise HTTPException(status_code=404,detail="chat not found")
+    if chat is None:
+        raise HTTPException(status_code=404,detail="chat not found")
 
-        session.delete(chat)
-        session.commit()
+    session.delete(chat)
+    session.commit()
 
     return {
         "message":"chat deleted successfully"
@@ -75,93 +84,96 @@ def delete_chat(chat_id:int,current_user:User=Depends(get_current_user)):
 @router.post('/{chat_id}/messages',response_model=CreateMessageResponse)
 def create_message(chat_id:int,
                    message:MessageCreate,
-                   current_user:User=Depends(get_current_user)):
-    with Session(engine) as  session:
-        chat=session.exec(
-            select(Chat).where(
-                (Chat.id==chat_id)&(Chat.user_id==current_user.id)
-            )
-        ).first()
+                   current_user:User=Depends(get_current_user),
+                   session:Session=Depends(get_session)):
 
-        if chat is None:
-            raise HTTPException(status_code=404,detail='chat not found')
-
-        new_message=Message(
-            chat_id=chat.id,
-            role="user",
-            content=message.content
+    chat=session.exec(
+        select(Chat).where(
+            (Chat.id==chat_id)&(
+            Chat.user_id==current_user.id)
         )
+    ).first()
 
-        session.add(new_message)
-        session.commit()
-        session.refresh(new_message)
+    if chat is None:
+        raise HTTPException(status_code=404,detail='chat not found')
+
+    new_message=Message(
+        chat_id=chat.id,
+        role="user",
+        content=message.content
+    )
+
+    session.add(new_message)
+    session.commit()
+    session.refresh(new_message)
     
-        messages=session.exec(
-            select(Message).where(
-                Message.chat_id==chat_id
-            ).order_by(Message.created_at.desc())
-            .limit(20)
-        ).all()
+    messages=session.exec(
+        select(Message).where(
+            Message.chat_id==chat_id
+        ).order_by(Message.created_at.desc())
+        .limit(20)
+    ).all()
 
-        messages.reverse()
+    messages.reverse()
 
         
-        try:
-            ai_response=ask_gemini_with_history(messages)
-        except RuntimeError as e:
-            raise HTTPException(status_code=503,detail=str(e))
-       
-        # ai_response=ask_gemini(message.content)
+    try:
+        ai_response=ask_gemini_with_history(messages)
+    except RuntimeError as e:
+        raise HTTPException(status_code=503,detail=str(e))
+    
+    # ai_response=ask_gemini(message.content)
 
-        ai_message=Message(
-            chat_id=chat.id,
-            role="model",
-            content=ai_response
-        )
+    ai_message=Message(
+        chat_id=chat.id,
+        role="model",
+        content=ai_response
+    )
 
-        session.add(ai_message)
-        session.commit()
-        session.refresh(ai_message)
+    session.add(ai_message)
+    session.commit()
+    session.refresh(ai_message)
 
-        result= {
-            "user_message": {
-                "id": new_message.id,
-                "chat_id": new_message.chat_id,
-                "role": new_message.role,
-                "content": new_message.content,
-                "created_at": new_message.created_at
-            },
-            "ai_message": {
-                "id": ai_message.id,
-                "chat_id": ai_message.chat_id,
-                "role": ai_message.role,
-                "content": ai_message.content,
-                "created_at": ai_message.created_at
-            }
+    result= {
+        "user_message": {
+            "id": new_message.id,
+            "chat_id": new_message.chat_id,
+            "role": new_message.role,
+            "content": new_message.content,
+            "created_at": new_message.created_at
+        },
+        "ai_message": {
+            "id": ai_message.id,
+            "chat_id": ai_message.chat_id,
+            "role": ai_message.role,
+            "content": ai_message.content,
+            "created_at": ai_message.created_at
         }
-        return result
+    }
+    return result
 
 @router.get("/{chat_id}/messages",response_model=list[MessageResponse])
 def get_messages(chat_id:int,
-                 current_user:User=Depends(get_current_user)):
+                 current_user:User=Depends(get_current_user),
+                 session:Session=Depends(get_session)):
 
-    with Session(engine) as session:
-        chat=session.exec(
-           select(Chat).where( 
-               (Chat.id==chat_id)&(Chat.user_id==current_user.id)
-           )
-        ).first()
+    
+    chat=session.exec(
+        select(Chat).where( 
+            (Chat.id==chat_id)&(Chat.user_id==current_user.id)
+        )
+    ).first()
 
-        if chat is None:
-            raise HTTPException(
-                status_code=404,detail="chat not found"
-            )
+    if chat is None:
+        raise HTTPException(
+            status_code=404,detail="chat not found"
+        )
 
-        messages=session.exec(
-            select(Message).where(
-                Message.chat_id==chat_id
-            )
-        ).all()
+    messages=session.exec(
+        select(Message).where(
+            Message.chat_id==chat_id
+        )
+    ).all()
 
     return messages
 
@@ -169,37 +181,38 @@ def get_messages(chat_id:int,
 def delete_message(
     chat_id: int,
     message_id: int,
-    current_user: User = Depends(get_current_user)
+    current_user: User = Depends(get_current_user),
+    session:Session=Depends(get_session)
 ):
-    with Session(engine) as session:
-        chat = session.exec(
-            select(Chat).where(
-                (Chat.id == chat_id) &
-                (Chat.user_id == current_user.id)
-            )
-        ).first()
+    
+    chat = session.exec(
+        select(Chat).where(
+            (Chat.id == chat_id) &
+            (Chat.user_id == current_user.id)
+        )
+    ).first()
 
-        if chat is None:
-            raise HTTPException(
-                status_code=404,
-                detail="Chat not found"
-            )
+    if chat is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Chat not found"
+        )
 
-        message = session.exec(
-            select(Message).where(
-                (Message.id == message_id) &
-                (Message.chat_id == chat_id)
-            )
-        ).first()
+    message = session.exec(
+        select(Message).where(
+            (Message.id == message_id) &
+            (Message.chat_id == chat_id)
+        )
+    ).first()
 
-        if message is None:
-            raise HTTPException(
-                status_code=404,
-                detail="Message not found"
-            )
+    if message is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Message not found"
+        )
 
-        session.delete(message)
-        session.commit()
+    session.delete(message)
+    session.commit()
 
     return {
         "message": "Message deleted successfully"
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,10 @@
+import pytest
+import uuid
+
 from fastapi.testclient import TestClient
 from sqlmodel import SQLModel, create_engine, Session
 from sqlalchemy.pool import StaticPool
+
 from app.main import app
 from app.database import get_session
 
@@ -22,6 +26,45 @@ app.dependency_overrides[get_session] = override_get_session
 SQLModel.metadata.create_all(test_engine)
 
 client = TestClient(app)
+
+@pytest.fixture
+def auth_headers():
+
+    unique_id=uuid.uuid4().hex
+    username=f"fixtureuser_{unique_id}"
+    email=f"{unique_id}@example.com"
+    
+    client.post(
+        "/auth/register",
+        json={
+            "username":'username',
+            'email':"email",
+            'password':'password123'
+        }
+    )
+
+    login_response=client.post(
+        '/auth/login',
+        data={
+            'username':'username',
+            'password':'password123'
+        }
+    )
+    token=login_response.json()['access_token']
+
+    return {
+        "Authorization":f"Bearer {token}"
+    }
+
+@pytest.fixture
+def mock_gemini(monkeypatch):
+    def fake_gemini(messages):
+
+        return "This is a fake Ai response"
+
+    monkeypatch.setattr(
+        "app.routes.chat.ask_gemini_with_history",fake_gemini
+    )
 
 def test_home():
     response=client.get("/")
@@ -142,7 +185,232 @@ def test_getme_without_token():
     response=client.get("/auth/me")
 
     assert response.status_code==401
+
+
+def test_create_chat(auth_headers):
+    response=client.post(
+        '/chat/',
+        params={
+            "title":"My first test chat"
+        },
+        headers=auth_headers
+    )
+
+    assert response.status_code==200
+
+    data=response.json()
+
+    assert data['title']=="My first test chat"
+    assert data['user_id'] is not None
+    assert data['id'] is not None
+
+
+def test_get_chats(auth_headers):
+    create_response=client.post(
+        '/chat',
+        params={
+            'title':'Chat One',
+        },
+        headers=auth_headers
+    )
+
+    assert create_response.status_code==200
+
+    response=client.get(
+        '/chat/',
+        headers=auth_headers
+    )
+
+    assert response.status_code==200
+
+    data=response.json()
+
+    assert len(data)>=1
+
+    titles=[chat['title'] for chat in data]
+
+    assert "Chat One" in titles
+
+
+def test_get_chat(auth_headers):
+    create_response=client.post(
+        '/chat/',
+        params={
+            'title':"specific chat"
+        },
+        headers=auth_headers
+    )
+
+    assert create_response.status_code==200
+    chat_id=create_response.json()['id']
+    response=client.get(
+        f"chat/{chat_id}",
+        headers=auth_headers
+    )
+
+    assert response.status_code==200
+
+    data=response.json()
+
+    assert data['id']==chat_id
+    assert data['title']=="specific chat"
+
+
+def test_delete_chat(auth_headers):
+    create_response = client.post(
+        "/chat/",
+        params={
+            "title": "Chat To Delete"
+        },
+        headers=auth_headers
+    )
+
+    assert create_response.status_code == 200
+
+    chat_id = create_response.json()["id"]
+
+    delete_response = client.delete(
+        f"/chat/{chat_id}",
+        headers=auth_headers
+    )
+
+    assert delete_response.status_code == 200
+
+    data = delete_response.json()
+
+    assert data["message"] == "chat deleted successfully"
+
+
+def test_get_messages(auth_headers):
+    create_response=client.post(
+        '/chat/',
+        params={
+            "title":'message test chat'
+        },
+        headers=auth_headers
+    )
+
+    assert create_response.status_code==200
+
+    chat_id=create_response.json()['id']
+
+    response=client.get(
+        f"/chat/{chat_id}/messages",
+        headers=auth_headers
+    )
+
+    assert response.status_code==200
+
+    data=response.json()
+    assert isinstance(data,list)
+    assert len(data)==0
+
+def test_create_message(auth_headers,mock_gemini):
     
 
+    create_response=client.post(
+        '/chat/',
+        params={
+            'title':'gemini test chat'
+        },
+        headers=auth_headers
+    )
+
+    assert create_response.status_code==200
+
+    chat_id=create_response.json()['id']
+
+    response=client.post(
+        f'/chat/{chat_id}/messages',
+        json={
+            'content':'hello'
+        },
+        headers=auth_headers
+    )
+
+    assert response.status_code==200
+    data=response.json()
+
+    assert "user_message" in data
+    assert 'ai_message' in data
+
+    assert data['user_message']['content']=='hello'
+    assert data['user_message']['role']=='user'
+
+    assert data['ai_message']['role']=='model'
+    assert data['ai_message']['content']=='This is a fake Ai response'
 
 
+def test_get_messages_after_create(auth_headers,mock_gemini):
+
+   
+    create_response=client.post(
+        "/chat/",
+        params={
+            "title":"History Test Chat"
+        },
+        headers=auth_headers
+    )
+
+    assert create_response.status_code==200
+    chat_id=create_response.json()['id']
+    message_response=client.post(
+        f'/chat/{chat_id}/messages',
+        json={
+            'content':'hello history'
+        },
+        headers=auth_headers
+    )
+
+    assert message_response.status_code==200
+
+    response=client.get(
+        f"/chat/{chat_id}/messages",
+        headers=auth_headers
+    )
+
+    assert response.status_code==200
+    data=response.json()
+    assert len(data)==2
+
+    assert data[0]['role']=='user'
+    assert data[0]['content']=='hello history'
+
+    assert data[1]['role']=='model'
+    assert data[1]['content']=='This is a fake Ai response'
+
+def test_delete_message(auth_headers,mock_gemini):
+    
+    create_response=client.post(
+        '/chat',
+        params={
+            'title':'delete message chat'
+        },
+        headers=auth_headers
+    )
+
+    assert create_response.status_code==200
+
+    chat_id=create_response.json()['id']
+
+    message_response=client.post(
+        f'/chat/{chat_id}/messages',
+        json={
+            'content':'message to delete'
+        },
+        headers=auth_headers
+    )
+
+    assert message_response.status_code==200
+
+    message_id=message_response.json()['user_message']['id']
+
+    delete_response=client.delete(
+        f'/chat/{chat_id}/messages/{message_id}',
+        headers=auth_headers
+    )
+
+    assert delete_response.status_code==200
+    data=delete_response.json()
+
+    assert data['message']=="Message deleted successfully"
